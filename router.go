7c01c95f3c4f640364151df1b202dbb846e56d49
Arne Bahlo <hallo@arne.me>
2014-08-01 11:03:31 +0200

    Add notFoundHandler, Use

--- router.go
+++ router.go
@@ -1,9 +1,23 @@
 package goat
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 // Router represents a router
 type Router struct {
 	middleware []Middleware
 	router     *httprouter.Router
+}
+
+// notFoundHandler handles (as you already know) the 404 error
+func (r *Router) notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement notFoundHandler
+}
+
+// Use adds middleware(s) to the router
+func (r *Router) Use(m ...Middleware) {
+	r.middleware = append(r.middleware, m...)
 }
