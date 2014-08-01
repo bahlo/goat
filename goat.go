63eb06bc5c2cd4ecbc09300344f06239134877ae
Arne Bahlo <hallo@arne.me>
2014-08-01 12:00:56 +0200

    Update example with goat.Params

--- goat.go
+++ goat.go
@@ -2,8 +2,10 @@ package goat
 
 import "github.com/julienschmidt/httprouter"
 
+// New creates a new Router and returns it
 func New() *Router {
 	r := &Router{}
+	r.prefix = "/"
 	r.router = httprouter.New()
 	r.router.NotFound = r.notFoundHandler
 
