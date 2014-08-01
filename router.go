78f9aba898448b95dfe5c1d5e697ff781769f328
Arne Bahlo <hallo@arne.me>
2014-08-01 11:55:33 +0200

    Add addRoute, methods (GET, POST, DELETE so far)

--- router.go
+++ router.go
@@ -10,6 +10,7 @@ import (
 type Router struct {
 	prefix string
 	router *httprouter.Router
+	index  map[string]string
 }
 
 // notFoundHandler handles (as you already know) the 404 error
@@ -21,3 +22,50 @@ func (r *Router) notFoundHandler(w http.ResponseWriter, req *http.Request) {
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.router.ServeHTTP(w, req)
 }
+
+// subPath returns the prefix of the router + the given path and eliminates
+// duplicate slashes
+func (r *Router) subPath(p string) string {
+	pre := r.prefix
+
+	if pre[:len(pre)-1] != "/" && p[:1] != "/" {
+		pre += "/"
+	}
+
+	return pre + p
+}
+
+// addRoute adds a route to the index and passes it over to the httprouter
+func (r *Router) addRoute(m, p, t string, fn httprouter.Handle) {
+	path := r.subPath(p)
+
+	// Add to index
+	if len(t) > 0 {
+		// TODO: Display total path including host
+		r.index[t] = path
+	}
+
+	r.router.Handle(m, path, fn)
+}
+
+// Get adds a GET route
+func (r *Router) Get(p, t string, fn httprouter.Handle) {
+	r.addRoute("GET", p, t, fn)
+}
+
+// Get adds a POST route
+func (r *Router) Post(p, t string, fn httprouter.Handle) {
+	r.addRoute("POST", p, t, fn)
+}
+
+// Get adds a DELETE route
+func (r *Router) Delete(p, t string, fn httprouter.Handle) {
+	r.addRoute("DELETE", p, t, fn)
+}
+
+// Get adds a PUT route
+func (r *Router) Put(p, t string, fn httprouter.Handle) {
+	r.addRoute("PUT", p, t, fn)
+}
+
+// TODO: Add PATCH, OPTIONS, HEAD?
